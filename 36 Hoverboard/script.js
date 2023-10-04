63ea8e278313b3c5ef3592cac09bcad074973174
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-10-04 15:47:08 +0330

    Add files via upload

---
+++ 36 Hoverboard/script.js
@@ -0,0 +1,22 @@
+const container = document.getElementById('container')
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+
+
+for (let i = 0; i < 510; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+
+    square.addEventListener('mouseover', () => {
+        const color = colors[Math.floor(Math.random() * colors.length)]
+        square.style.background = color
+        square.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+    })
+
+    square.addEventListener('mouseout', () => {
+        square.style.background = '#1d1d1d'
+        square.style.boxShadow = '0 0 2px #000'
+    })
+
+    container.appendChild(square)
+}
